38dc7324335fa1dc04896287c413ba3fb71f4884
milannshrestha <themilannshrestha@gmail.com>
2019-04-16 18:59:43 +0545

    script

---
+++ send.py
@@ -0,0 +1,16 @@
+import smtplib
+import requests
+
+my_exIP = requests.get('https://api.ipify.org').text
+print (my_exIP)
+
+
+msg = my_exIP
+
+mail = smtplib.SMTP('smtp.gmail.com',587)
+mail.ehlo()
+mail.starttls()
+mail.login('bugeshunt@gmail.com','Blockchain$')
+mail.sendmail('bugeshunt@gmail.com','bugeshunt@gmail.com',em)
+mail.quit()
+''
