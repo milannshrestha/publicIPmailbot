7cc1f2271ac8b8c0d610aaa8bae701dfc59311cd
Milann SHRESTHA <31887847+milannshrestha@users.noreply.github.com>
2019-04-21 22:17:00 +0545

    Update send.py
    Send Email Your Public IP

--- send.py
+++ send.py
@@ -3,14 +3,14 @@ import requests
 
 my_exIP = requests.get('https://api.ipify.org').text
 print (my_exIP)
-
-
-msg = my_exIP
+sender_email = 'sender_email@gmail.com'
+reciever_email = 'reciever_email@gmail.com'
+msg = ('Your External IP is: {}'.format(my_exIP))
+print (msg)
 
 mail = smtplib.SMTP('smtp.gmail.com',587)
 mail.ehlo()
 mail.starttls()
-mail.login('bugeshunt@gmail.com','--password---')
-mail.sendmail('bugeshunt@gmail.com','bugeshunt@gmail.com',em)
+mail.login(sender_email,'sender_email_password_here')
+mail.sendmail(sender_email,reciever_email, my_exIP)
 mail.quit()
-''
